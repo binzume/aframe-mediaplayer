bca32f84ea5d607625f6b024c8bbc89f92fbcf6b
Kousuke Kawahira <kawahira@binzume.net>
2019-11-12 21:18:49 +0900

    xycanvas

--- mediaplayer.js
+++ mediaplayer.js
@@ -579,6 +579,30 @@ AFRAME.registerComponent('media-controller', {
 	}
 });
 
+
+AFRAME.registerComponent('xycanvas', {
+    schema: {
+        width: { default: 16 },
+        height: { default: 16 }
+    },
+    init() {
+        this.canvas = document.createElement("canvas");
+
+        // to avoid texture cache conflict in a-frame.
+        this.canvas.id = "_CANVAS" + Math.random();
+        let src = new THREE.CanvasTexture(this.canvas);
+        this.updateTexture = () => {
+            src.needsUpdate = true;
+        };
+
+        this.el.setAttribute('material', { shader: "flat", npot: true, src: src, transparent: true });
+    },
+    update() {
+        this.canvas.width = this.data.width;
+        this.canvas.height = this.data.height;
+    }
+});
+
 AFRAME.registerComponent('stereo-texture', {
 	schema: {
 		mode: { default: "side-by-side", oneOf: ["side-by-side", "top-and-bottom"] },
